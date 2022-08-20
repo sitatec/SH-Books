64ab8c72ad02abe1174bcfc0711aab4c598b061c
Sita Bérété <sita.berete.3@gmail.com>
2022-08-20 19:15:55 +0000

    Setup kubernetes, Skaffold and Google cloud GKE dev cluster

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -4,6 +4,10 @@ const app = Express();
 
 app.use(Express.json());
 
+app.get('/api/users/currentuser', (request, response) => {
+  response.send('Hi there!');
+});
+
 app.listen(8080, () => {
   console.log("Listening on 8080!");
 });
