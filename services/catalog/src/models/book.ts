606f93dedcbd6235f7a6e0300854add76ab0e7fb
Sita Bérété <sita.berete.3@gmail.com>
2022-09-30 18:31:26 +0000

    [catalog] Create Book model

---
+++ services/catalog/src/models/book.ts
@@ -0,0 +1,58 @@
+import { clearObjectOwnProperties } from "@shbooks/common";
+import { Document, model, Model, Schema, SchemaOptions } from "mongoose";
+
+export interface Book {
+  title: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+  sellerId: string;
+}
+
+export interface BookDocument extends Document, Book {}
+
+export interface BookModel extends Model<BookDocument> {
+  build(book: Book): BookDocument;
+  insert(book: Book): Promise<BookDocument>;
+}
+
+// TODO: move to `common` package
+const RequiredStringSchema = {
+  type: String,
+  required: true,
+};
+
+const toJson: SchemaOptions = {
+  toJSON: {
+    transform(document: BookDocument, returnValue) {
+      clearObjectOwnProperties(returnValue); // remove mongo specific properties and naming conventions
+      returnValue.id = document.id;
+      returnValue.title = document.title;
+      returnValue.description = document.description;
+      returnValue.imageUrl = document.imageUrl;
+      returnValue.sellerId = document.sellerId;
+      returnValue.price = document.price;
+    },
+  },
+};
+
+const bookSchema = new Schema(
+  {
+    title: RequiredStringSchema,
+    description: RequiredStringSchema,
+    imageUrl: RequiredStringSchema,
+    sellerId: RequiredStringSchema,
+    price: {
+      type: Number,
+      require: true,
+    },
+  },
+  toJson
+);
+
+bookSchema.statics.build = (book: Book) => new BookCollection(book);
+bookSchema.statics.insert = (book: Book) => BookCollection.create(book);
+
+const BookCollection = model<BookDocument, BookModel>("Book", bookSchema);
+
+export default BookCollection;
